3faa38090ac2696017397cec6e1c8ee4caa7f502
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2024-10-30 10:36:11 +0100

    Mettre à Jour le Formulaire popup au niveau des Champs et fonction du bouton Annuler

---
+++ assets/js/popup.js
@@ -0,0 +1,7 @@
+function openPopup() {
+    document.getElementById('add-task-popup').classList.remove('hidden');
+}
+
+function closePopup() {
+    document.getElementById('add-task-popup').classList.add('hidden');
+}
